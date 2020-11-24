9e4432eeeb665046f2ca9ab63dfb4db62188f688
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2020-11-24 16:41:28 +0000

    Update reg_complete.java

--- ATM_project/reg_complete.java
+++ ATM_project/reg_complete.java
@@ -5,48 +5,58 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class reg_complete {
+public class reg_complete extends JFrame implements ActionListener{
+
+    JButton retToHome;
+    JLabel regMsg, regMsg2;
+
+
+
+    reg_complete() {
+
+        regMsg = new JLabel("Thank you for creating an account please return to the");
+        regMsg.setHorizontalAlignment(SwingConstants.CENTER);
+        regMsg.setFont(new Font("Arial", Font.BOLD, 13));
+
+        regMsg2 = new JLabel(" homepage to login.");
+        regMsg2.setHorizontalAlignment(SwingConstants.CENTER);
+        regMsg2.setFont(new Font("Arial", Font.BOLD, 13));
+
+        retToHome = new JButton("RETURN TO HOME");
+        retToHome.setFocusable(false);
+        retToHome.setFont(new Font("Arial", Font.PLAIN, 13));
+
+        setLayout(null);
+
+        regMsg.setBounds(50, 10, 350, 30);
+        add(regMsg);
+
+        regMsg2.setBounds(150, 30, 150, 30);
+        add(regMsg2);
+
+        retToHome.setBounds(125,80,225,30);
+        add(retToHome);
+
+
+
+        retToHome.addActionListener(this);
+
+
+        setTitle("REGISTRATION COMPLETE");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(475,200);
+        setLocation(500,100);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        setVisible(false);
+        new homepage().setVisible(true);
+    }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        register_complete();
+        new reg_complete().setVisible(true);
 
-    }
-
-    public static void register_complete() {
-
-        JFrame frame = new JFrame("REGISTRATION COMPLETE");
-        frame.setBounds(100,100,450,200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(null);
-
-        JLabel regMsg = new JLabel("Thank you for creating an account please return to the");
-        regMsg.setHorizontalAlignment(SwingConstants.CENTER);
-        regMsg.setFont(new Font("Arial", Font.BOLD, 13));
-        regMsg.setBounds(50, 10, 350, 30);
-        frame.getContentPane().add(regMsg);
-
-        JLabel regMsg2 = new JLabel(" homepage to login.");
-        regMsg2.setHorizontalAlignment(SwingConstants.CENTER);
-        regMsg2.setFont(new Font("Arial", Font.BOLD, 13));
-        regMsg2.setBounds(150, 40, 150, 30);
-        frame.getContentPane().add(regMsg2);
-
-        JButton retToHome = new JButton("RETURN TO HOME");
-        retToHome.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-                frame.setVisible(false);
-                homepage home = new homepage();
-                home.homescreen();
-            }
-        });
-        retToHome.setFont(new Font("Arial", Font.PLAIN, 13));
-        retToHome.setBounds(125, 100, 200, 30);
-        frame.getContentPane().add(retToHome);
-
-
-
-
-        frame.setVisible(true);
     }
 }
