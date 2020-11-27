7632d61f295f6b04391d9f1fee9f97aff5e21edb
Jonathan-Challenger <62345423+Jonathan-Challenger@users.noreply.github.com>
2020-11-27 13:32:14 +0000

    Update SignUp.java

--- ATM_project/SignUp.java
+++ ATM_project/SignUp.java
@@ -7,11 +7,9 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 public class SignUp extends JFrame implements ActionListener{
-    private static JTextField emailfield;
-    private static JTextField cardNoField;
-    private static JTextField pinField;
-    private static JTextField balanceField;
-    JButton backToHome, createAcc, clearBut;
+    private static JTextField emailfield, titleField, firstField, lastField;
+    JButton backToHome, next, clearBut;
+    JLabel regTitle, per, email, title, first, last;
 
 
 
@@ -24,65 +22,71 @@ public class SignUp extends JFrame implements ActionListener{
         backToHome.setBounds(100, 375, 300, 35);
         add(backToHome);
 
-        createAcc = new JButton("CREATE ACCOUNT");
-        createAcc.setBounds(100, 275, 300, 35);
-        add(createAcc);
+        next = new JButton("NEXT");
+        next.setBounds(100, 275, 300, 35);
+        add(next);
 
         clearBut = new JButton("CLEAR");
         clearBut.setBounds(100, 325, 300, 35);
         add(clearBut);
 
-        JLabel regTitle = new JLabel("REGISTER");
+        regTitle = new JLabel("REGISTER");
         regTitle.setFont(new Font("Arial", Font.BOLD, 18));
         regTitle.setHorizontalAlignment(SwingConstants.CENTER);
-        regTitle.setBounds(168, 26, 150, 50);
+        regTitle.setBounds(168, 20, 150, 50);
         add(regTitle);
 
-        JLabel email = new JLabel("Email :");
+        per = new JLabel("Personal Details: ");
+        per.setFont(new Font("Arial", Font.PLAIN, 14));
+        per.setHorizontalAlignment(SwingConstants.CENTER);
+        per.setBounds(168, 50, 150, 50);
+        add(per);
+
+        email = new JLabel("Email :");
         email.setFont(new Font("Arial", Font.BOLD, 12));
         email.setBounds(50, 100, 60, 30);
         add(email);
 
-        JLabel cardNo = new JLabel("Card no. :");
-        cardNo.setFont(new Font("Arial", Font.BOLD, 12));
-        cardNo.setBounds(50, 140, 125, 30);
-        add(cardNo);
+        title = new JLabel("Title :");
+        title.setFont(new Font("Arial", Font.BOLD, 12));
+        title.setBounds(50, 140, 125, 30);
+        add(title);
 
-        JLabel pin = new JLabel("PIN :");
-        pin.setFont(new Font("Arial", Font.BOLD, 12));
-        pin.setBounds(50, 180, 125, 30);
-        add(pin);
+        first = new JLabel("First Name :");
+        first.setFont(new Font("Arial", Font.BOLD, 12));
+        first.setBounds(50, 180, 125, 30);
+        add(first);
 
-        JLabel balance = new JLabel("Balance :");
-        balance.setFont(new Font("Arial", Font.BOLD, 12));
-        balance.setBounds(50, 220, 125, 30);
-        add(balance);
+        last = new JLabel("Last Name :");
+        last.setFont(new Font("Arial", Font.BOLD, 12));
+        last.setBounds(50, 220, 125, 30);
+        add(last);
 
         emailfield = new JTextField();
         emailfield.setBounds(120, 100, 300, 30);
         add(emailfield);
         emailfield.setColumns(10);
 
-        cardNoField = new JTextField();
-        cardNoField.setBounds(120, 140, 300, 30);
-        add(cardNoField);
-        cardNoField.setColumns(10);
+        titleField = new JTextField();
+        titleField.setBounds(120, 140, 300, 30);
+        add(titleField);
+        titleField.setColumns(10);
 
-        pinField = new JTextField();
-        pinField.setBounds(120, 180, 300, 30);
-        add(pinField);
-        pinField.setColumns(10);
+        firstField = new JTextField();
+        firstField.setBounds(120, 180, 300, 30);
+        add(firstField);
+        firstField.setColumns(10);
 
-        balanceField = new JTextField();
-        balanceField.setBounds(120, 220, 300, 30);
-        add(balanceField);
-        balanceField.setColumns(10);
+        lastField = new JTextField();
+        lastField.setBounds(120, 220, 300, 30);
+        add(lastField);
+        lastField.setColumns(10);
 
         clearBut.addActionListener(this);
-        createAcc.addActionListener(this);
+        next.addActionListener(this);
         backToHome.addActionListener(this);
 
-        setTitle("REGISTER");
+        setTitle("SIGN UP");
         setSize(500,500);
         setLocation(500,100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -94,32 +98,32 @@ public class SignUp extends JFrame implements ActionListener{
 
         if (ae.getSource()==clearBut) {
             emailfield.setText("");
-            pinField.setText("");
-            cardNoField.setText("");
-            balanceField.setText("");
+            firstField.setText("");
+            titleField.setText("");
+            lastField.setText("");
 
         }
         else if (ae.getSource()==backToHome){
             setVisible(false);
             new homepage().setVisible(true);
         }
-        else if (ae.getSource()==createAcc){
+        else if (ae.getSource()==next){
             String a = emailfield.getText();
-            String b = cardNoField.getText();
-            String c = pinField.getText();
-            String d = balanceField.getText();
+            String b = titleField.getText();
+            String c = firstField.getText();
+            String d = lastField.getText();
 
             try {
-                if (emailfield.getText().equals("") || cardNoField.getText().equals("") || pinField.getText().equals("") || balanceField.getText().equals("")){
+                if (emailfield.getText().equals("") || titleField.getText().equals("") || firstField.getText().equals("") || lastField.getText().equals("")){
                     JOptionPane.showMessageDialog(null, "Please fill all required fields.");
                 }
                 else {
                     conn c1 = new conn();
-                    String q1 = "insert into login values('"+a+"','"+b+"', '"+c+"', '"+d+"')";
+                    String q1 = "insert into login values(null, '"+a+"','"+b+"', '"+c+"', '"+d+"', null, null, null)";
                     c1.s.executeUpdate(q1);
 
                     setVisible(false);
-                    new reg_complete().setVisible(true);
+                    new SignUp2().setVisible(true);
 
                 }
             } catch (SQLException throwables) {
